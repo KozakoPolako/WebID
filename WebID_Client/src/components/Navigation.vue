ebf6e4e8e3fcd8dd9e5b7ef8a6bd09bbd6e5d95b
KozakoPolako <dariusz.dabrowski1998@gmail.com>
2021-09-05 12:51:53 +0200

    start building layout

--- WebID_Client/src/components/Navigation.vue
+++ WebID_Client/src/components/Navigation.vue
@@ -1,9 +1,29 @@
 <template>
-  <nav>
+  <nav class="overflow-hidden">
       <v-app-bar
-      color="blue"
+      app
+      scroll-target=""
+      flat
+      color="transparent"
+      outlined
+      dense
       >
+        <v-spacer />
+        <v-btn
+          outlined
+        >
+          click me</v-btn>
+        <div  class="px-1"></div>
+        <v-btn
+          outlined
+        >
+          click me</v-btn>
+        <v-spacer />
       </v-app-bar>
+      <v-sheet
+      if="scrolling-techniques-7"
+      class="overflow-y-auto"
+      ></v-sheet>
   </nav>
 </template>
 
---
+++ WebID_Client/src/views/AddDocument.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container>
+    <v-row>
+        <v-col xs="12" ld="6">
+            <v-btn
+            >
+            clickMe
+            </v-btn>
+            <v-btn
+            >
+            clickM232
+            </v-btn>
+        </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
