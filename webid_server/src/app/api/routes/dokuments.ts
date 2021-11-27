08c16d493515796394452ee225097fb43c9351e2
KozakoPolako <dariusz.dabrowski1998@gmail.com>
2021-11-27 12:01:55 +0100

    add photo storage in database and some code refactor

--- webid_server/src/app/api/routes/dokuments.ts
+++ webid_server/src/app/api/routes/dokuments.ts
@@ -1,96 +1,126 @@
-import express, { Router, Request, Response } from "express"
-import multer from "multer"
+import express, { Router, Request, Response } from "express";
+import multer from "multer";
 import DowodOsobistyPL from "../../../rec/dowodOsoistyPL";
 import mkdirp from "mkdirp";
 import fs from "fs";
-import { mongoController } from "../../app"
-
-
+import { mongoController } from "../../app";
 
 const router = express.Router();
 
-const adress = "http://localhost:3000"
+const adress = "http://localhost:3000";
 
 const storageDir = "./test/";
 
 const storage = multer.diskStorage({
-    async destination(req,file,callback) {
-
-        let dir = storageDir;
-        try {
-           await mkdirp(dir) 
-           callback(null,dir)
-        }catch {
-
-            callback(new Error("Nie udało się utworzyć pliku"),dir)
-        }
-        
-    },
-    filename(req,file,callback) {
-        callback(null, Date.now() + file.originalname)
+  async destination(req, file, callback) {
+    let dir = storageDir;
+    try {
+      await mkdirp(dir);
+      callback(null, dir);
+    } catch {
+      callback(new Error("Nie udało się utworzyć pliku"), dir);
     }
-})
-const limits = (sizeMB:number) => {
-    return {fileSize: 1024 * 1024 * sizeMB}
-    
-}
-
+  },
+  filename(req, file, callback) {
+    callback(null, Date.now() + file.originalname);
+  },
+});
+const limits = (sizeMB: number) => {
+  return { fileSize: 1024 * 1024 * sizeMB };
+};
 
 //type filefilter = Pick<multer.Options,"fileFilter">["fileFilter"]
 
 const upload = multer({
-    storage: storage, 
-    limits: limits(10),
-    fileFilter: (req, file, callback) => {
-        if(file.mimetype.split("/")[0] === 'image'){
-            callback(null, true);
-        }else {
-            callback(null, false);
-        }
+  storage: storage,
+  limits: limits(10),
+  fileFilter: (req, file, callback) => {
+    if (file.mimetype.split("/")[0] === "image") {
+      callback(null, true);
+    } else {
+      callback(null, false);
     }
+  },
 });
 
-router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message:"TODO"
+router.get("/", (req, res, next) => {
+  res.status(200).json({
+    message: "TODO",
+  });
+});
+//pobierz zdjęcie z dowodu
+router.get("/pl/dowod/zdjecie/:photo/:docID", async (req, res, next) => {
+  const document = await mongoController.getDowod(req.params.docID);
+  if (document) {
+    switch (req.params.photo) {
+      case "face":
+        mongoController.sendDowodPhotoByID(document.faceID, res);
+        break;
+      case "front":
+        mongoController.sendDowodPhotoByID(document.frontID, res);
+        break;
+      case "back":
+        mongoController.sendDowodPhotoByID(document.backID, res);
+        break;
+      default:
+        res.status(400).json({
+          message: "Nieprawidłowy Parametr",
+        });
+        break;
+    }
+  } else {
+    res.status(404).json({
+      message: "Nie znaleziono dokumentu",
     });
+  }
 });
 
-router.post('/pl/dowod', upload.array('dowodImage',2) , async (req, res, next) => {
+// wysałanie dowodu do rozpoznania
+router.post(
+  "/pl/dowod",
+  upload.array("dowodImage", 2),
+  async (req, res, next) => {
     //console.log(req.files);
-    
-    if(req.files && req.files.length == 2){
-        try {
-            var files = [];
-            var fileKeys = Object.keys(req.files);
-            //const file = fs.readFileSync("D:\\OneDrive - Wojskowa Akademia Techniczna\\Obrazy\\Praca Inżynierska zdj\\Dowod-Osobisty-2015.jpg");
-            // @ts-ignore 
-            const temp = await DowodOsobistyPL.getDocumentFromPhoto(req.files[0].path, req.files[1].path); 
-            console.log("Skończyłem")
-            console.dir(temp)
-            // @ts-ignore
-            const frontfilename = req.files[0].filename.split('.')[0]
-            // @ts-ignore
-            const backfilename = req.files[1].filename.split('.')[0]
-            
-            console.log( await mongoController.insertDocument(temp, frontfilename,backfilename))
-            res.status(200).json({
-                dowod: temp,
-                faceURL: `${adress}/${frontfilename}/face.jpg`
-            })
-        }catch(e) {
-            console.log(e);
-            res.status(404).json({
-                message: 'Nie udało się odczytać danych ze zdjęcia'
-            });
-        }
-        
 
-        
+    if (req.files && req.files.length == 2) {
+      try {
+        var files = [];
+        var fileKeys = Object.keys(req.files);
+        //const file = fs.readFileSync("D:\\OneDrive - Wojskowa Akademia Techniczna\\Obrazy\\Praca Inżynierska zdj\\Dowod-Osobisty-2015.jpg");
+        // @ts-ignore
+        const temp = await DowodOsobistyPL.getDocumentFromPhoto(
+          // @ts-ignore
+          req.files[0].path,
+          // @ts-ignore
+          req.files[1].path
+        );
+        console.log("Skończyłem");
+        console.dir(temp);
+        // @ts-ignore
+        const frontfilename = req.files[0].filename.split(".")[0];
+        // @ts-ignore
+        const backfilename = req.files[1].filename.split(".")[0];
+
+        const record = await mongoController.insertDocument(
+          temp,
+          frontfilename,
+          backfilename
+        );
+        const recordID = record?.insertedId.toString();
+        res.status(200).json({
+          dowod: temp,
+          faceURL: `${adress}/dokuments/pl/dowod/zdjecie/face/${recordID}`,
+          frontURL: `${adress}/dokuments/pl/dowod/zdjecie/front/${recordID}`,
+          backURL: `${adress}/dokuments/pl/dowod/zdjecie/fback/${recordID}`,
+        });
+      } catch (e) {
+        console.log(e);
+        res.status(404).json({
+          message: "Nie udało się odczytać danych ze zdjęcia",
+        });
+      }
     }
-        
-
-    
-});
+  }
+);
 
 export default router;
--- webid_server/src/mongoController/mongoController.ts
+++ webid_server/src/mongoController/mongoController.ts
@@ -1,12 +1,37 @@
+import { ObjectID } from "bson";
+import { Response } from "express";
 import fs from "fs";
-import { Collection, Db, GridFSBucket, GridFSBucketWriteStream, MongoClient } from "mongodb";
+import {
+  Collection,
+  Db,
+  GridFSBucket,
+  GridFSBucketReadStream,
+  GridFSBucketWriteStream,
+  MongoClient,
+  Timestamp,
+} from "mongodb";
+import { resolve } from "path/posix";
+import { Dowod } from "../rec/dowodOsoistyPL";
 
 const CONNECTION_STRING = `mongodb://${process.env.MONGO_LOGIN}:${process.env.MONGO_PASSWORD}@webid-db`;
+
+interface documentData {
+  documentData: Dowod;
+  creationDate: Date;
+}
+
+export interface DowodPLDocument {
+  faceID: ObjectID;
+  frontID: ObjectID;
+  backID: ObjectID;
+  saved: boolean;
+  dataHistory: documentData[];
+}
 
 class Mongo {
   client: MongoClient;
   database: Db | undefined;
-  documentsCol: Collection | undefined;
+  documentsCol: Collection<DowodPLDocument> | undefined;
   usersCol: Collection<Document> | undefined;
   photosBucket: GridFSBucket | undefined;
 
@@ -27,7 +52,7 @@ class Mongo {
   test() {
     console.log("test test");
   }
-  async insertDocument(doc: Object, frontName: string, backName: string) {
+  async insertDocument(doc: Dowod, frontName: string, backName: string) {
     try {
       await this.client.connect();
       this.database = this.client.db("WebID");
@@ -36,33 +61,39 @@ class Mongo {
         bucketName: "DowodPhotos",
       });
 
-
-      // const fs  = await this.photosBucket.openUploadStream(`${frontName}face.jpg`)
-
-      // const face = fs
-      //   .createReadStream(`./temporary/${frontName}/face.jpg`)
-      //   .pipe(this.photosBucket.openUploadStream(`${frontName}face.jpg`));
-      // const front = fs
-      //   .createReadStream(`./temporary/${frontName}/document.jpg`)
-      //   .pipe(this.photosBucket.openUploadStream(`${frontName}document.jpg`));
-      // const back = fs
-      //   .createReadStream(`./temporary/${backName}/document.jpg`)
-      //   .pipe(this.photosBucket.openUploadStream(`${backName}document.jpg`));
-
-      const face = await this._uploadFile(`./temporary/${frontName}/face.jpg`,`${frontName}face.jpg`,this.photosBucket);
-      const front = await this._uploadFile(`./temporary/${frontName}/document.jpg`,`${frontName}document.jpg`,this.photosBucket);
-      const back = await this._uploadFile(`./temporary/${backName}/document.jpg`,`${backName}document.jpg`,this.photosBucket);
+      const face = await this._uploadFile(
+        `./temporary/${frontName}/face.jpg`,
+        `${frontName}face.jpg`,
+        this.photosBucket
+      );
+      const front = await this._uploadFile(
+        `./temporary/${frontName}/document.jpg`,
+        `${frontName}document.jpg`,
+        this.photosBucket
+      );
+      const back = await this._uploadFile(
+        `./temporary/${backName}/document.jpg`,
+        `${backName}document.jpg`,
+        this.photosBucket
+      );
 
       console.log(
         `face id : ${face.id}  front id : ${front.id} back id : ${back.id}`
       );
 
-      return await this.documentsCol.insertOne({
+      const dowod: DowodPLDocument = {
         faceID: face.id,
         frontID: front.id,
         backID: back.id,
-        data: doc,
-      });
+        saved: false,
+        dataHistory: [
+          {
+            creationDate: new Date(),
+            documentData: doc,
+          },
+        ],
+      };
+      return await this.documentsCol.insertOne(dowod);
     } catch (error) {
       console.log(`Insert Error: ${error}`);
       return null;
@@ -70,24 +101,97 @@ class Mongo {
       await this.client.close();
     }
   }
-  async _uploadFile(path: string, filename: string, bucket: GridFSBucket) : Promise<GridFSBucketWriteStream>  {
-    return new Promise( (resolve, reject) => {
+  async _uploadFile(
+    path: string,
+    filename: string,
+    bucket: GridFSBucket
+  ): Promise<GridFSBucketWriteStream> {
+    return new Promise((resolve, reject) => {
       let file: GridFSBucketWriteStream;
-      const stream = fs.createReadStream(path)
+      const stream = fs.createReadStream(path);
       stream.on("open", () => {
-        file = stream.pipe(bucket.openUploadStream(filename))
-        file.on("finish",() => {
-          resolve(file)
-        })
+        file = stream.pipe(bucket.openUploadStream(filename));
+        file.on("finish", () => {
+          resolve(file);
+        });
         file.on("error", (error) => {
-          reject(error)
-        })
-      })
+          reject(error);
+        });
+      });
       stream.on("error", (error) => {
-        reject(error)
-      })
+        reject(error);
+      });
     });
+  }
+  async getDowod(id: string): Promise<DowodPLDocument | undefined> {
+    try {
+      await this.client.connect();
+      this.database = this.client.db("WebID");
+      this.documentsCol = this.database.collection("dokumenty");
+
+      const doc = await this.documentsCol.findOne<DowodPLDocument>({
+        _id: new ObjectID(id),
+      });
+      console.log(doc);
+
+      return doc ? doc : undefined;
+    } catch (error) {
+      console.log(`Find Error: ${error}`);
+    } finally {
+      await this.client.close();
+    }
+  }
+  async sendDowodPhotoByID(id: ObjectID, res: Response): Promise<void> {
+    try {
+      await this.client.connect();
+      this.database = this.client.db("WebID");
+      this.documentsCol = this.database.collection("dokumenty");
+      this.photosBucket = new GridFSBucket(this.database, {
+        bucketName: "DowodPhotos",
+      });
+      
+      const downloadStrem = this.photosBucket.openDownloadStream(id);
+      await this._downloadFile(downloadStrem,res);
+    } catch (error) {
+    } finally {
+      await this.client.close();
+    }
+  }
+  async _downloadFile(
+    stream: GridFSBucketReadStream,
+    res: Response
+  ): Promise<void> {
+    {
+      return new Promise((resolve, reject) => {
+        stream.on("data", (data) => {
+          res.status(200).write(data);
+        });
+
+        stream.on("error", (error) => {
+          res.status(404).json({ message: "Nie można pobrać obrazka" });
+          reject(error);
+        });
+
+        stream.on("end", () => {
+          res.end();
+          resolve();
+        });
+      });
+    }
   }
 }
 
 export default new Mongo();
+
+// function snippets
+
+// try {
+//   await this.client.connect();
+//   this.database = this.client.db("WebID");
+//   this.documentsCol = this.database.collection("dokumenty");
+
+// } catch (error) {
+
+// } finally {
+//   await this.client.close();
+// }
