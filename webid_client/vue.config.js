9b7af56e04e12be3b79f244f1f0353d2638cfa23
KozakoPolako <dariusz.dabrowski1998@gmail.com>
2021-12-10 19:20:49 +0100

    devproxyConfig

--- webid_client/vue.config.js
+++ webid_client/vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ]
-}
+  transpileDependencies: ["vuetify"],
+  devServer: {
+    proxy: {
+      "^/api": {
+        target: "https://192.168.0.246:3000",
+        pathRewrite: { "^/api/": "/api/" },
+        changeOrigin: true,
+      },
+    },
+  },
+};
